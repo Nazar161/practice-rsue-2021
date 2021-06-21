5b9a98e0e700fd344676dd009880854de9a60460
Nazaret <nazaret155@mail.ru>
2021-06-21 15:13:57 +0300

    changed JSON api

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,7 @@
 export default class OurService {
     
     getResource = async (url) => {
-        const res = await fetch(`https://practice-rsue-2021.herokuapp.com/api/${url}`);
+        const res = await fetch(`https://practice-rsue-2021.herokuapp.com/api${url}`);
 
         if(!res.ok) {
             throw new Error(`Could not fetch ${url}` +
